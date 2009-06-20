e7a3b0f2083d8f6ab3be7bd4cb727b4b1a944498
Simon Kjikàqawej Côté <simon.raven@gmail.com>
2009-06-20 09:17:11 -0400

    modify some html elements to be xhtml

--- IkiWiki/Plugin/recaptcha.pm
+++ IkiWiki/Plugin/recaptcha.pm
@@ -34,7 +34,7 @@ sub formbuilder_setup (@) {
 <script type="text/javascript" src="http://api.recaptcha.net/challenge?k=$pubkey"></script>
 <noscript>
     <iframe src="http://api.recaptcha.net/noscript?k=$pubkey"
-        height="300" width="500" frameborder="0"></iframe><br>
+        height="300" width="500" frameborder="0"></iframe><br />
     <textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
     <input type="hidden" name="recaptcha_response_field" value="manual_challenge" />
 </noscript>
@@ -44,7 +44,7 @@ EOTAG
 <script type="text/javascript" src="https://api-secure.recaptcha.net/challenge?k=$pubkey"></script>
 <noscript>
     <iframe src="https://api-secure.recaptcha.net/noscript?k=$pubkey"
-        height="300" width="500" frameborder="0"></iframe><br>
+        height="300" width="500" frameborder="0"></iframe><br />
     <textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
     <input type="hidden" name="recaptcha_response_field" value="manual_challenge" />
 </noscript>
