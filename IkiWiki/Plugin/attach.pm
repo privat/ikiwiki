0143fe0fc271787ee2d5ca68d257cc8e8a1ae21e
Simon Kjikàqawej <simon.raven@gmail.com>
2009-12-20 00:17:40 -0500

    final touches, then build!
    Signed-off-by: Simon Kjikàqawej <simon.raven@gmail.com>

--- IkiWiki/Plugin/attach.pm
+++ IkiWiki/Plugin/attach.pm
@@ -8,7 +8,7 @@ use IkiWiki 3.00;
 our ($dir, $max_kbs, $srcdir_max_kbs, $mime_strategy, %mime_allow, %mime_deny, $DEFAULT_MAX_KBS, %want_form);
 
 sub import { #{{{
-    hook(type => "checkconfig",  id=>"attach",   call => \&checkconfig);
+    hook(type => "checkconfig",  id => "attach", call => \&checkconfig);
     hook(type => "sessioncgi",   id => "attach", call => \&attach);
     hook(type => "pagetemplate", id => "attach", call => \&pagetemplate);
     hook(type => "preprocess",   id => "attach", call => \&preprocess);
