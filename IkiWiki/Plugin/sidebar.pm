cf209f851a3e6a2913dfae235dab704645a806a4
Jean Privat <jean@pryen.org>
2010-04-23 12:50:13 -0400

    sidebar: allows more than one sidebar
    Signed-off-by: Jean Privat <jean@pryen.org>

--- IkiWiki/Plugin/sidebar.pm
+++ IkiWiki/Plugin/sidebar.pm
@@ -10,6 +10,7 @@ use IkiWiki 3.00;
 
 sub import {
 	hook(type => "getsetup", id => "sidebar", call => \&getsetup);
+	hook(type => "preprocess", id => "sidebar", call => \&preprocess);
 	hook(type => "pagetemplate", id => "sidebar", call => \&pagetemplate);
 }
 
@@ -19,12 +20,20 @@ sub getsetup () {
 			safe => 1,
 			rebuild => 1,
 		},
+		active_sidebars => {
+			type => "string",
+			example => qw(sidebar banner footer),
+			description => "Which sidebars must be activated and processed.",
+			safe => 1,
+			rebuild => 1
+		},
 }
 
-sub sidebar_content ($) {
+sub sidebar_content ($$) {
 	my $page=shift;
+	my $sidebar=shift;
 	
-	my $sidebar_page=bestlink($page, "sidebar") || return;
+	my $sidebar_page=bestlink($page, $sidebar) || return;
 	my $sidebar_file=$pagesources{$sidebar_page} || return;
 	my $sidebar_type=pagetype($sidebar_file);
 	
@@ -49,11 +58,17 @@ sub pagetemplate (@) {
 
 	my $page=$params{page};
 	my $template=$params{template};
-	
-	if ($template->query(name => "sidebar")) {
-		my $content=sidebar_content($page);
-		if (defined $content && length $content) {
-		        $template->param(sidebar => $content);
+
+	my @sidebars;
+	if (defined $config{active_sidebars} && length $config{active_sidebars}) { @sidebars = @{$config{active_sidebars}}; }
+	else { @sidebars = qw(sidebar); }
+
+	foreach my $sidebar (@sidebars) {
+		if ($template->query(name => $sidebar)) {
+			my $content=sidebar_content($page, $sidebar);
+			if (defined $content && length $content) {
+				$template->param($sidebar => $content);
+			}
 		}
 	}
 }
